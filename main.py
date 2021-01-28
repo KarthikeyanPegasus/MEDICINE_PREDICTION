ea6b050af566b6d3faae8d87d835891e4f439c29
karthikeyan <62192093+KarthikeyanPegasus@users.noreply.github.com>
2021-01-28 12:01:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import json
+import ast
+import pickle
+from flask import Flask, request, jsonify
+from model_files.ml_model import dosage_prediction
+
+app = Flask("dosage_prediction")
+
+@app.route('/',methods = ['POST'])
+def predict():
+    detai = (request.get_json())
+    print(detai)
+    de = json.dumps(detai)
+    print(de)
+    detail = json.loads(de)
+    print(detail)
+    deta = detail["list"]
+    print(deta)
+    details = ast.literal_eval(deta)
+    print(details, type(details))
+    pre = []
+    pre.append(details[0])
+    pre.append(details[1])
+    prediction = list([pre])
+    print(prediction)
+
+    with open('./model_files/dosage_prediction.bin', 'rb') as f_in:
+        dosemodel = pickle.load(f_in)
+        f_in.close()
+
+    dosepredict = dosemodel.predict(prediction)
+
+    dosage = dosepredict[0]
+    response = {
+
+        'medicine_prediction': list(dosage)
+    }
+
+
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=True, host = '127.0.0.1', port = 8080)
